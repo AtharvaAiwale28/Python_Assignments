8ce78ba2c55c8764b654d5c9ae5c3ce19dfd9059
Atharva Aiwale <atharva.aiwale28@gmail.com>
2026-06-03 13:08:31 +0530

    Arithmetic operators

---
+++ assignment2.py
@@ -0,0 +1,22 @@
+#Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder
+
+a = 12
+b = 28
+
+#addation
+print (a+b)
+
+#subtract
+print(a-b)
+
+#multiply
+print(a*b)
+
+#divide 
+print(a/b)
+
+#remainder
+print(a%b)
+
+#power
+print(a**2)
